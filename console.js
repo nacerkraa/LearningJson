4459c4d1be222748b0e39ca199b8acd4f381c62a
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-06-24 17:46:43 +0000

    write file js

---
+++ console.js
@@ -0,0 +1,18 @@
+file1 = {
+    "name": "nacer",
+    "age" : 12
+}
+
+file2 = {
+    "name": "ahmed",
+    "age" : 12
+}
+
+file3 = {
+    "name": "rain",
+    "age" : 12
+}
+
+console.log(file1.name);
+console.log(file2.name);
+console.log(file3.name);
